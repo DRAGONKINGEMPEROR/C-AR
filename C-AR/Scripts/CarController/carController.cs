cf5a2238fd2fff79427c9b974e1a201715fc3be3
DRAGONKINGEMPEROR <htssoe999@gmail.com>
2018-07-03 19:28:14 +0530

    Add files via upload

---
+++ C-AR/Scripts/CarController/carController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carController : MonoBehaviour {
+
+	Animator anim;
+ 	public static carController instance;
+
+	
+	void Awake(){
+		if (instance == null) {
+			instance = this;
+		}
+	}
+
+	
+	void Start () {
+ 
+		for (int i = 0; i < transform.childCount; ++i) {
+			
+			
+ 			if (transform.GetChild (i).gameObject.name == gameController.currentSelectedCar) {
+				transform.GetChild (i).gameObject.SetActive (true);
+
+				
+				anim = transform.GetChild (i).gameObject.GetComponent<Animator> ();
+			} else {
+				
+				transform.GetChild (i).gameObject.SetActive (false);
+			}
+ 		}
+
+		 
+	}
+
+	
+ 	public void triggerAnimation(string action){
+		anim.SetTrigger (action);
+	}
+
+	public void showMessage(){
+		//TODO
+	}
+
+}
